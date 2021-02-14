584b91d4880bfcbc41bdb86c1d5945c315482e4c
Henrique Vieira <64736691+PedroHdev@users.noreply.github.com>
2021-02-14 16:55:57 -0300

    v2

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,29 @@
 <template>
-  <div id="app">
-    <div v-for="(pokemon, index) in pokemons" :key="index">
-      <Pokemon :nome="pokemon.name" :url="pokemon.url" :num="index+1" ></Pokemon>
+  <div class="container" id="app">
+    <div class="columns mt-4">
+
+      <div class="column is-half is-offset-one-quarter" align="center">
+
+        <h1 class="title">Pokedex</h1>
+
+      </div>
+
+    </div>
+
+    <div class="columns mt-1">
+
+      <div class="column is-half is-offset-one-quarter" align="center">
+
+        <input class="input is-primary" v-model="busca" type="text" placeholder="Buscar Pokemon pelo Nome">
+
+      </div>
+
+    </div>
+
+    <div class="row">
+      <div v-for="(pokemon, index) in resultadoBusca" :key="index">
+        <Pokemon :nome="pokemon.name" :url="pokemon.url" :num="index+1" ></Pokemon>
+      </div>
     </div>
   </div>
 </template>
@@ -17,20 +39,35 @@ export default {
   },
   data(){
     return {
-      pokemons: []
+      pokemons: [],
+      busca: ''
     }
   },
   created(){
+
     axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0").then((data) => {
+
       this.pokemons = data.data.results
       
     }).catch((err) => {
       console.log(err)
     })
+
+  },
+  computed: {
+    resultadoBusca(){
+      if(this.busca == '' || this.busca == ' '){
+        return this.pokemons
+      }else{
+        return this.pokemons.filter(pokemon => pokemon.name == this.busca.toLowerCase())
+      }
+    }
   }
 }
 </script>
 
-<style>
-
+<style scoped>
+  button{
+    width: 100%;
+  }
 </style>
--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -1,20 +1,74 @@
 <template>
-    <div align="center">
+    <div @mouseup="change" class="column is-half is-offset-one-quarter" align="center">
+        <div class="card">
+            <div class="card-image">
+                <figure>
+                    <img :src="pokemon.image" :alt="nome">
+                </figure>
+            </div>
+            <div class="card-content">
 
-        <h1 class="title">{{ num }} {{ nome }}</h1>
-        <small class="subtitle">{{ url }}</small>
-        <hr>
+                <div class="media">
+                    <div class="media-content">
+                        <p class="title is-4">{{ num }} - {{ nome | upper}}</p>
+                        <p class="subtitle is-6">{{ pokemon.type }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
 
     </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
     name: "Pokemon",
     props: {
         nome: String,
         url: String,
         num: Number
+    },
+    filters: {
+        upper(value){
+            return value[0].toUpperCase() + value.slice(1)
+        }
+    },
+    data(){
+        return {
+            pokemon: {
+                type: '',
+                front: '',
+                back: '',
+                image: ''
+            }
+        }
+    },
+    methods: {
+        change(){
+
+            if(this.pokemon.image == this.pokemon.back){
+                this.pokemon.image = this.pokemon.front
+            }else{
+                this.pokemon.image = this.pokemon.back
+            }
+
+
+            
+        }
+    },
+    created(){
+        axios.get(this.url).then((data) => {
+
+            this.pokemon.type = data.data.types[0].type.name
+            this.pokemon.front = data.data.sprites.front_default
+            this.pokemon.back = data.data.sprites.back_default
+            this.pokemon.image = this.pokemon.front
+
+        }).catch((err) => {
+            console.log(err)
+        })
     }
 }
 </script>
